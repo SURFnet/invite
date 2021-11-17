6ad77f05d3e2da22c7071482a039b61fd52f43ea
Peter Havekes <peter@havekes.eu>
2021-11-17 11:49:56 +0100

    Guests are Users

--- docs/makeAPIdoc.sh
+++ docs/makeAPIdoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-redoc-cli bundle ../specs/guestappapi.yaml --options.theme.colors.primary.main=orange -o ./api/index.html
+redoc-cli bundle ../specs/invite.yaml --options.theme.colors.primary.main=orange -o ./api/index.html
 
